3572c2000633e1b97100374a918e4ebec6162110
DanielHe <danielhe930203@gmail.com>
2016-05-17 12:24:40 +0800

    first commit

---
+++ DHMicroBlog/DHMicroBlog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHMicroBlog
+//
+//  Created by DanielHe on 5/17/16.
+//  Copyright © 2016 DanielHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DHMicroBlog/DHMicroBlog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DHMicroBlog
+//
+//  Created by DanielHe on 5/17/16.
+//  Copyright © 2016 DanielHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
